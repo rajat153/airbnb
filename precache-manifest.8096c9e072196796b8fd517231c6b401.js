322c382796161c1b6c8a4116a87f8bc97fc1f1bd
rajat153 <rajat.force1@gmail.com>
2022-06-23 20:05:50 +0530

    Updates

---
+++ precache-manifest.8096c9e072196796b8fd517231c6b401.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8caa11fe3cf2c1d2e83",
+    "url": "/airbnb/static/css/main.3d062445.chunk.css"
+  },
+  {
+    "revision": "a8caa11fe3cf2c1d2e83",
+    "url": "/airbnb/static/js/main.a8caa11f.chunk.js"
+  },
+  {
+    "revision": "527a97298922cb139f85",
+    "url": "/airbnb/static/js/1.527a9729.chunk.js"
+  },
+  {
+    "revision": "7255aa12d1beea1063ef",
+    "url": "/airbnb/static/js/runtime~main.7255aa12.js"
+  },
+  {
+    "revision": "251ecabf7757051efd304fa61dc9d4e7",
+    "url": "/airbnb/static/media/Group.251ecabf.png"
+  },
+  {
+    "revision": "57264508d865539a80130b8efa953c9f",
+    "url": "/airbnb/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/airbnb/precache-manifest.8096c9e072196796b8fd517231c6b401.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/airbnb/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
